c82b3a626c110fe2505526fdbb8a09168cc0956b
A-Bush <a.bushlyakov@gmail.com>
2016-04-03 16:41:55 +0300

    UserMailer test

--- pages/MailController.php
+++ pages/MailController.php
@@ -15,6 +15,7 @@ class MailController
     private static $register = "register";
     private static $info = "info";
     private static $manager = "manager";
+    private static $header = "Content - type: text / html; charset = utf - 8\r\n";
 
     public static function registerMail($data)
     {
@@ -28,9 +29,9 @@ JOIN modifiers ON modifiers.id = courseinfo.modifier
 JOIN course ON course.id = courseinfo.course_id
 LEFT JOIN shedule ON shedule.course_id = courseinfo.course_id AND shedule.start > CURDATE() AND shedule.modifier = courseinfo.modifier
 WHERE modifiers.id = '{$modifier_id}' AND course.id='{$course_id}'");
-        $info = $courseinfo[0]["course_name"] . " " .$courseinfo[0]["modifier_name"]. " .";
-        if(strlen($courseinfo[0]["start"]) > 0) {
-            $info .= "Стартуем: " . $courseinfo[0]["start"]." ";
+        $info = $courseinfo[0]["course_name"] . " " . $courseinfo[0]["modifier_name"] . " .";
+        if (strlen($courseinfo[0]["start"]) > 0) {
+            $info .= "Стартуем: " . $courseinfo[0]["start"] . " ";
         }
         $msg = "
                 Имя:\t" . $data["name"] . "\n
@@ -40,10 +41,22 @@ WHERE modifiers.id = '{$modifier_id}' AND course.id='{$course_id}'");
                 Комментарий:\t" . $data["comment"] . "\n
                 ";
 
-        $headers = "Content - type: text / html; charset = utf - 8 \r\n";
-        $headers .= "From: Отправитель <" . $mail . ">\r\n";
+        $headers = self::$header . 'From: <' . $mail . '>' . "\r\n" . 'To: <' . $to . '>' . "\r\n";
         mail($to, $subj, $msg, $headers);
+        self::userMail($mail, $info);
+    }
 
+    public static function userMail($to, $info)
+    {
+        $subj = "Регистрация на курс от Webcamp.";
+        $msg = "
+                ололо!\r\n
+                вы записались на курс " . $info . ". \r\n
+                мы вам когда-нибудь позвоним или не позвоним.\r\n
+                Щастяздоровля
+                ";
+        $headers = self::$header.'From: <' . self::$register . self::$webcampDomain . '>' . "\r\n" . 'To: <' . $to . '>' . "\r\n";
+        mail($to, $subj, $msg, $headers);
     }
 
 }
