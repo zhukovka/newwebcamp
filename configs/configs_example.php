5a5185ff830c51579e6f4ecf4455a480a0f55296
A-Bush <a.bushlyakov@gmail.com>
2016-04-19 21:50:05 +0300

    merged all branches

--- configs/configs_example.php
+++ configs/configs_example.php
@@ -5,7 +5,7 @@
  * Date: 4/16/16
  * Time: 19:03
  */
-define (DB_USER, "root");
-define (DB_PASSWORD, "root");
-define (DB_DATABASE, "webcamp");
-define (DB_HOST, 'mysql:dbname=webcamp;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;charset=utf8');
+define ("DB_USER", "root");
+define ("DB_PASSWORD", "root");
+define ("DB_DATABASE", "webcamp");
+define ("DB_HOST", 'mysql:dbname=webcamp;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;charset=utf8');
