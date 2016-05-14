adc506f30db79dd390998afb5a59c591a8b81a0d
zhukovka <helen.zhukova@gmail.com>
2016-05-14 23:11:23 +0300

    phone

--- js/courses-ng/enroll.js
+++ js/courses-ng/enroll.js
@@ -1,64 +1,65 @@
 angular.module('Enroll', [])
-    .controller('EnrollController', ['$scope', '$http', '$httpParamSerializerJQLike',
-        function ($scope, $http, $httpParamSerializerJQLike) {
-        $scope.how = ['Google', 'Посоветовали', 'Facebook', 'Другое'];
-        $scope.enrollSuccess = false;
-        $scope.enrollError = false;
-        $scope.showEnrollForm = true;
-        $scope.sqlError = false;
-
-        var student = {
-            how: $scope.how[0],
-            phone: "38"
-        };
-        $scope.student = angular.copy(student);
-        $scope.$on('closeModal', function(){
+    .controller('EnrollController', ['$scope', '$filter', '$http', '$httpParamSerializerJQLike',
+        function ($scope, $filter, $http, $httpParamSerializerJQLike) {
+            $scope.how = ['Google', 'Посоветовали', 'Facebook', 'Другое'];
             $scope.enrollSuccess = false;
             $scope.enrollError = false;
             $scope.showEnrollForm = true;
             $scope.sqlError = false;
-        });
-        function successCallback(response) {
-            ga('send', 'event', 'Регистрация', 'Записаться');
-            if(response.data && response.data.sqlError){
-                $scope.sqlError = true;
-            }else{
-                $scope.enrollSuccess = true;
+            $scope.regexPhone = /\+\d{1,}\(?\d{2,}\)?\d{3}-?\d{2,}/;
+            var student = {
+                how: $scope.how[0],
+                phone: "38"
+            };
+            $scope.student = angular.copy(student);
+            $scope.$on('closeModal', function () {
+                $scope.enrollSuccess = false;
+                $scope.enrollError = false;
+                $scope.showEnrollForm = true;
                 $scope.sqlError = false;
+            });
+            function successCallback(response) {
+                ga('send', 'event', 'Регистрация', 'Записаться');
+                if (response.data && response.data.sqlError) {
+                    $scope.sqlError = true;
+                } else {
+                    $scope.enrollSuccess = true;
+                    $scope.sqlError = false;
+                }
+                $scope.enrollError = false;
+                $scope.showEnrollForm = false;
+                console.log(response);
             }
-            $scope.enrollError = false;
-            $scope.showEnrollForm = false;
-            console.log(response);
-        }
-        function errorCallback(response) {
-            $scope.enrollSuccess = false;
-            $scope.enrollError = true;
-            $scope.showEnrollForm = false;
-            $scope.sqlError = false;
-            console.log(response);
-        }
 
-        $scope.postEnroll = function (student, form) {
-            form = form || null;
-            if(form.$valid){
-                student.course_id = $scope.enrollSchedule.course_id;
-                student.modifier_id = $scope.enrollSchedule.modifier_id;
-                student.hash = student.course_id + "" + student.modifier_id + "" + student.phone;
-                console.log(student.hash);
-                form.$setPristine();
-                form.$setUntouched();
-
-                $http({
-                    method: 'POST',
-                    url: '/enroll',
-                    data: $httpParamSerializerJQLike(student),
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                }).then(successCallback, errorCallback);
+            function errorCallback(response) {
+                $scope.enrollSuccess = false;
+                $scope.enrollError = true;
+                $scope.showEnrollForm = false;
+                $scope.sqlError = false;
+                console.log(response);
             }
-        };
-    }])
+
+            $scope.postEnroll = function (student, form) {
+                form = form || null;
+                if (form.$valid) {
+                    student.course_id = $scope.enrollSchedule.course_id;
+                    student.modifier_id = $scope.enrollSchedule.modifier_id;
+                    student.modifier_text = $filter('modifier')($scope.enrollSchedule.modifier_name);
+                    student.hash = student.course_id + "" + student.modifier_id + "" + student.phone;
+                    form.$setPristine();
+                    form.$setUntouched();
+
+                    $http({
+                        method: 'POST',
+                        url: '/enroll',
+                        data: $httpParamSerializerJQLike(student),
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    }).then(successCallback, errorCallback);
+                }
+            };
+        }])
     .directive('enrollModal', [function () {
         return {
             restrict: 'E',
