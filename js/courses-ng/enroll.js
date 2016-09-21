31c084f1c789e570cc49817e9871923db03b82b5
A-Bush <a.bushlyakov@gmail.com>
2016-09-22 02:33:07 +0300

    adwords target deleted

--- js/courses-ng/enroll.js
+++ js/courses-ng/enroll.js
@@ -20,7 +20,6 @@ angular.module('Enroll', [])
             });
             function successCallback(response) {
                 ga('send', 'event', 'Регистрация', 'Записаться');
-                goog_report_conversion();
                 if (response.data && response.data.sqlError) {
                     $scope.sqlError = true;
                 } else {
